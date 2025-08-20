3cb61c0cf4f8c8084d8458e08133c3bd52fe6e01
NeyOkaou <143295893+NeyOkaou@users.noreply.github.com>
2025-08-20 21:06:47 +0200

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -22223,7 +22223,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		contestType: "Cool",
 	},
 	brainfreeze: {
-		num: 42011,
+		num: -105,
 		accuracy: 100,
 		basePower: 120,
 		category: "Physical",
@@ -22234,7 +22234,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		recoil: [33, 100],
 		secondary: {
 			chance: 10,
-			status: 'frz',
+			status: 'frb',
 		},
 		target: "normal",
 		type: "Ice",
