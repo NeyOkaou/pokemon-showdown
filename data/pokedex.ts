c370056e25365f5e1354a05945afdf0e5ddfa94a
NeyOkaou <143295893+NeyOkaou@users.noreply.github.com>
2025-09-17 02:43:36 +0200

    Update pokedex.ts

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -20408,7 +20408,7 @@ export const Pokedex: import('../sim/dex-species').SpeciesDataTable = {
 		types: ["Normal"],
 		gender: "N",
 		baseStats: { hp: 75, atk: 100, def: 75, spa: 95, spd: 75, spe: 100 },
-		abilities: { 0: "Normalize", H:"Color Change"}, 
+		abilities: { 0: "Normalize", H:"Protean"}, 
 		heightm: 0.3,
 		weightkg: 4,
 		color: "Gray",
